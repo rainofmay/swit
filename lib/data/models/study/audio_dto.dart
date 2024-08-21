2fef0fd086717abadb1cbb496a153d13edf4b794
우창현 <woo@uchanghyeon-ui-MacBookPro.local>
2024-08-21 10:29:14 +0900

    [UPDATE] 음악

---
+++ lib/data/models/study/audio_dto.dart
@@ -0,0 +1,16 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'audio_dto.freezed.dart';
+part 'audio_dto.g.dart';
+
+@freezed
+class AudioDTO with _$AudioDTO {
+  const factory AudioDTO({
+    required int id,
+    required String name,
+    required String theme,
+    required String audioURL,
+  }) = _AudioDTO;
+
+  factory AudioDTO.fromJson(Map<String, dynamic> json) => _$AudioDTOFromJson(json);
+}
