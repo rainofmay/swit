d60db338b438870f6b281d86564e41e864dc9f33
woo <mist00966@gmail.com>
2024-09-19 19:59:42 +0900

    update

--- lib/features/mate/presentation/view/post_tab.dart
+++ lib/features/mate/presentation/view/post_tab.dart
@@ -1,14 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:swit/features/mate/presentation/viewmodel/mate_view_model.dart';
+import 'package:swit/features/mate/presentation/widgets/calendar.dart';
 import 'package:swit/features/mate/presentation/widgets/post_it.dart';
 
-class PostScreen extends StatelessWidget {
+class PostScreen extends GetView<MateViewModel> {
   const PostScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        PostIt(),
+        Calendar(),
       ],
     );
   }
