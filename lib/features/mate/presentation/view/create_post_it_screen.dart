bd98b08a7081c6dd5a1de42504eda026371a95aa
woo <mist00966@gmail.com>
2024-10-22 23:44:54 +0900

    [UPDATE] post it add

--- lib/features/mate/presentation/view/create_post_it_screen.dart
+++ lib/features/mate/presentation/view/create_post_it_screen.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:intl/intl.dart';
 import 'package:swit/features/mate/presentation/viewmodel/post_tab_view_model.dart';
+import 'package:swit/features/mate/presentation/widgets/post_it_note.dart';
 import 'package:swit/shared/constant/color_box.dart';
 import 'package:swit/shared/widgets/custom_back_appbar.dart';
 import 'package:swit/shared/widgets/custom_gap.dart';
@@ -15,38 +16,93 @@ class CreatePostItScreen extends GetView<PostTabViewModel> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: CustomScaffold(
-      appBar: CustomBackAppBar(
-        appbarTitle: '서로에게 한 마디',
-        isLeading: true,
-        isCenterTitle: true,
-        actions: [
-          IconButton(onPressed: () {}, icon: const Icon(CupertinoIcons.checkmark_alt))
-        ],
-      ),
-      body: Center(
-          child: Container(
-        width: 300,
-        height: 300,
-        decoration: BoxDecoration(
-          color: ColorBox.secondColor,
-        ),
-        child: Column(
-          children: [
-            Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
-              Text('user name'),
-              Text(DateFormat('yyyy.MM.dd').format(controller.selectedDate)),
-            ]),
-            CustomGap(24),
-            Obx(() => CustomTextFormField(
-              isReadOnly: false,
-              controller: controller.postItController,
-              onChanged: (val) => controller.updatePostIt(),
-              maxLength: 300,
-            )),
+      child: CustomScaffold(
+        appBar: CustomBackAppBar(
+          appbarTitle: '서로에게 한 마디',
+          isLeading: true,
+          isCenterTitle: true,
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(CupertinoIcons.checkmark_alt))
           ],
         ),
-      )),
-    ));
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: Column(
+              children: [
+                const CustomGap(24),
+                // PostItNote 위젯 사용
+                Center(
+                  child: PostItNote(),
+                ),
+                const CustomGap(24),
+                // 날짜 선택기
+                Container(
+                  padding: const EdgeInsets.all(16.0),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(12),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.grey.withOpacity(0.2),
+                        spreadRadius: 1,
+                        blurRadius: 3,
+                        offset: const Offset(0, 1),
+                      ),
+                    ],
+                  ),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        '날짜 선택',
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const CustomGap(8),
+                      InkWell(
+                        onTap: () async {
+                          final DateTime? picked = await showDatePicker(
+                            context: context,
+                            initialDate: controller.selectedDate,
+                            firstDate: DateTime(2000),
+                            lastDate: DateTime(2101),
+                          );
+                          if (picked != null) {
+                            controller.updateSelectedDate(picked);
+                          }
+                        },
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Obx(() => Text(
+                                  DateFormat('yyyy년 MM월 dd일')
+                                      .format(controller.selectedDate),
+                                  style: const TextStyle(
+                                    fontSize: 14,
+                                    color: Colors.black87,
+                                  ),
+                                )),
+                            const Icon(
+                              Icons.calendar_today,
+                              size: 20,
+                              color: Colors.black54,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
